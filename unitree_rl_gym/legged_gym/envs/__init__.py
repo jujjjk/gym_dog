71d7ab8956a61f13240c3aee9315baa33c687858
nszb <nszb@example.com>
2026-07-24 19:23:34 +0800

    fix recovered Gym playback imports

--- unitree_rl_gym/legged_gym/envs/__init__.py
+++ unitree_rl_gym/legged_gym/envs/__init__.py
@@ -112,7 +112,7 @@ from legged_gym.envs.h1_2.h1_2_config import H1_2RoughCfg, H1_2RoughCfgPPO
 from legged_gym.envs.h1_2.h1_2_env import H1_2Robot
 from legged_gym.envs.g1.g1_config import G1RoughCfg, G1RoughCfgPPO
 from legged_gym.envs.g1.g1_env import G1Robot
-from base.legged_robot import LeggedRobot
+from .base.legged_robot import LeggedRobot
 from legged_gym.utils.task_registry import task_registry as task_registry
 
 task_registry.register("go2", LeggedRobot, GO2RoughCfg(), GO2RoughCfgPPO())
